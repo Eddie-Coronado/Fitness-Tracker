a6e0d3879e327bfd2f5721ac7a73c86470efd0b6
Eddie Coronado <coronadoeddiej@gmail.com>
2021-07-16 14:00:06 -0700

    testing

--- server.js
+++ server.js
@@ -14,9 +14,10 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workouts", { 
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
+mongoose.connect("mongodb://localhost/fitness-Tracker", { 
+  useNewUrlParser: true,
+  useFindAndModify: false,
+  useUnifiedTopology: true,
 
 });
 
