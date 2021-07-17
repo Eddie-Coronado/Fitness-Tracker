6e633b53324fba16eb6fec832f25da467512eadd
Eddie Coronado <coronadoeddiej@gmail.com>
2021-07-16 19:43:20 -0700

    update again

--- server.js
+++ server.js
@@ -1,41 +1,61 @@
+// const express = require("express");
+// const logger = require("morgan");
+// const mongoose = require("mongoose");
+// // const routes = require('./routes/api-routes');
+
+
+// const PORT = process.env.PORT || 3000;
+
+// const app = express();
+// app.use(logger("dev"));
+
+
+// app.use(express.urlencoded({ extended: true }));
+// app.use(express.json());
+
+// app.use(express.static("public"));
+
+
+// // mongoose.connect("mongodb://localhost/fitness-Tracker", { 
+// //   useNewUrlParser: true,
+// //   useFindAndModify: false,
+// //   useUnifiedTopology: true,
+// // })
+
+// const database = require("./config/keys").mongoURI;
+
+// mongoose
+//   .connect(
+//     database,
+//     { useNewUrlParser: true, useUnifiedTopology: true }
+//   )
+//   .then(() => console.log("MongoDB successfully connected"))
+//   .catch(err => console.log(err));
+
+// // app.use(routes)
+// app.use(require('./routes/api-routes.js'));
+// app.use(require('./routes/html-routes.js'));
+
+// app.listen(PORT, () => {
+//   console.log(`App running on port ${PORT}!`);
+// });
+
 const express = require("express");
-const logger = require("morgan");
 const mongoose = require("mongoose");
-// const routes = require('./routes/api-routes');
-
-
+const logger = require("morgan");
 const PORT = process.env.PORT || 3000;
-
 const app = express();
 app.use(logger("dev"));
-
-
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
 app.use(express.static("public"));
-
-
-// mongoose.connect("mongodb://localhost/fitness-Tracker", { 
-//   useNewUrlParser: true,
-//   useFindAndModify: false,
-//   useUnifiedTopology: true,
-// })
-
-const database = require("./config/keys").mongoURI;
-
-mongoose
-  .connect(
-    database,
-    { useNewUrlParser: true, useUnifiedTopology: true }
-  )
-  .then(() => console.log("MongoDB successfully connected"))
-  .catch(err => console.log(err));
-
-// app.use(routes)
-app.use(require('./routes/api-routes.js'));
-app.use(require('./routes/html-routes.js'));
-
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/fitness-tracker", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false
+});
+app.use(require("./routes/api-routes.js"));
+app.use(require("./routes/html-routes.js"));
 app.listen(PORT, () => {
-  console.log(`App running on port ${PORT}!`);
+    console.log(`Server is running on: http://localhost:${PORT}/ \n`);
 });
